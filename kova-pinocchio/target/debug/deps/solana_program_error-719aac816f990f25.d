5f7f13436421f1347c134612b80d246dcc87c564
HZNPrince <hznprincework@gmail.com>
2026-03-13 18:24:50 +0530

    Remove target from being tracked

--- kova-pinocchio/target/debug/deps/solana_program_error-719aac816f990f25.d
+++
@@ -1,5 +0,0 @@
-/Users/aster27/Desktop/github/Side_Projects/Kova/kova-pinocchio/target/debug/deps/solana_program_error-719aac816f990f25.d: /Users/aster27/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/solana-program-error-3.0.0/src/lib.rs
-
-/Users/aster27/Desktop/github/Side_Projects/Kova/kova-pinocchio/target/debug/deps/libsolana_program_error-719aac816f990f25.rmeta: /Users/aster27/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/solana-program-error-3.0.0/src/lib.rs
-
-/Users/aster27/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/solana-program-error-3.0.0/src/lib.rs:
